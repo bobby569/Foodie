88edb789dd1b23033c7a3b369db9c9adaa695cb6
bobby569 <zhan2175@purdue.edu>
2018-01-30 08:46:19 -0500

    add layout

--- imports/components/App.js
+++ imports/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Layout from './template/Layout';
 import Home from './Home';
 import Recipe from './Recipe';
 import Search from './Search';
@@ -10,14 +11,16 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<BrowserRouter>
-					<Switch>
-						<Route path="/profile" component={UserProfile} />
-						<Route path="/recipe" component={Recipe} />
-						<Route path="/search" component={Search} />
-						<Route path="/" component={Home} />
-					</Switch>
-				</BrowserRouter>
+				<Layout>
+					<BrowserRouter>
+						<Switch>
+							<Route path="/profile" component={UserProfile} />
+							<Route path="/recipe" component={Recipe} />
+							<Route path="/search" component={Search} />
+							<Route path="/" component={Home} />
+						</Switch>
+					</BrowserRouter>
+				</Layout>
 			</div>
 		);
 	}
--- imports/components/UserProfile.js
+++ imports/components/UserProfile.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class UserProfile extends Component {
 	render() {
-		return <div>Here is user</div>;
+		return <div>User profile</div>;
 	}
 }
