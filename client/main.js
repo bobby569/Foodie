e403a7446cee771d068f5329224164acd81f16ec
bobby569 <zhan2175@purdue.edu>
2018-01-29 23:30:37 -0500

    add router, main files setup

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
+import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
-// import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
 import App from '../imports/components/App';
 
 Meteor.startup(() => {
--- imports/components/App.js
+++ imports/components/App.js
@@ -1,13 +1,23 @@
-import 'antd/dist/antd.css';
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import Home from './Home';
+import Recipe from './Recipe';
+import Search from './Search';
+import UserProfile from './UserProfile';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Button type="primary">primary</Button>
+				<BrowserRouter>
+					<Switch>
+						<Route path="/profile" component={UserProfile} />
+						<Route path="/recipe" component={Recipe} />
+						<Route path="/search" component={Search} />
+						<Route path="/" component={Home} />
+					</Switch>
+				</BrowserRouter>
 			</div>
 		);
 	}
