e828792aeb815f2ba9dde899c8854a6a90fd9795
Jiayi Kou <jiayikou@pal-nat186-20-167.itap.purdue.edu>
2018-02-01 12:57:51 -0500

    Add avatar for profile page

--- imports/components/Profile.js
+++ imports/components/Profile.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Avatar } from 'antd';
+import { Image } from 'react-bootstrap';
+import Dropzone from 'react-dropzone';
 
 export default class Profile extends Component {
-  render() {
-    return <div>User profile</div>;
-  }
+	register(event) {
+		event.preventDefault();
+	}
+	handleUpload() {}
+
+	render() {
+		let user = this.props.user;
+		return (
+			<div>
+				<Dropzone onDrop={this.handleUpload.bind(this)} className="dropzone">
+					<div className="avatar" />
+				</Dropzone>
+			</div>
+		);
+	}
 }
