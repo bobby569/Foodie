a7678905bd908238bb80102ae1dc0fbe62939af9
bobby569 <zhan2175@purdue.edu>
2018-02-08 12:28:15 -0500

    add detail recipe code template

--- imports/components/Recipe.js
+++ imports/components/Recipe.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Class extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			id: 0,
+			data: null
+		};
+	}
+
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.setState({ id });
+		// axios.get('url').then(({ data }) => {
+		// 	this.setState({ data });
+		// });
+	}
+
 	render() {
+<<<<<<< HEAD
 		return <div>Here are the saved recipes</div>;
+=======
+		// if (!data) return null;
+
+		return <div>{this.props.match.params.id}</div>;
+>>>>>>> 85cc6ec... add detail recipe code template
 	}
 }
