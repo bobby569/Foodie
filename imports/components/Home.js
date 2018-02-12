75e40ddfbf6cf883765e691829d288808c36aa1e
Claudiazhaoya <sun515@purdue.edu>
2018-02-12 18:07:49 -0500

    tab space fixed

--- imports/components/Home.js
+++ imports/components/Home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div className="home-page">
-          <div className="darkgrey">
-            <div className="info">
-              <h3>Welcome to Foodie!</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className="home-page">
+					<div className="darkgrey">
+						<div className="info">
+							<h3>Welcome to Foodie!</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
