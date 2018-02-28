60c1f38903812181b3834fe5ec61a0b87af53fff
wei170 <guocheng@guochengwei.com>
2018-02-27 21:18:14 -0500

    * Move API calls to the server * Add view counts when visit a recipe detail page

---
+++ imports/api/recipes.method.js
@@ -0,0 +1,37 @@
+import { check } from 'meteor/check';
+import { HTTP } from 'meteor/http';
+import axios from 'axios';
+import { API, URI_BASE_RETR } from '../components/util/constant.js';
+
+Meteor.methods({
+	'recipes.detailData': api_id => {
+		check(api_id, String);
+
+		const res = Recipes.find({ api_id: api_id }).fetch();
+		if (res.length === 0) {
+			const url = `${API}&r=${URI_BASE_RETR}${api_id}`;
+			HTTP.get(url, (err, res) => {
+				if (!err) {
+					const tmp = JSON.stringify(res.data[0]);
+					Recipes.insert({
+						api_id: api_id,
+						api_data: tmp
+					});
+
+					return Recipes.findOne({ api_id: api_id });
+				}
+			});
+		} else {
+			Recipes.update(
+				{ api_id: api_id },
+				{
+					$inc: {
+						viewCounts: 1
+					}
+				}
+			);
+
+			return Recipes.findOne({ api_id: api_id });
+		}
+	}
+});
