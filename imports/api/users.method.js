c914239b6551465b8df3ad68348061bedac3786d
kjy1997 <kouj@purdue.edu>
2018-03-04 15:38:19 -0500

    Finish saved recipes

--- imports/api/users.method.js
+++ imports/api/users.method.js
@@ -8,5 +8,23 @@ Meteor.methods({
 				}
 			}
 		);
+	},
+	'users.getSavedRecipe': userId => {
+		const user = Meteor.users.findOne(
+			{
+				_id: userId
+			},
+			{
+				'profile.savedRecipes': 1
+			}
+		);
+		const saved = user.profile.savedRecipes;
+		const recipeArr = [];
+		saved.forEach(api_id => {
+			let recipe = Recipes.findOne({ api_id });
+			let api_data = JSON.parse(recipe.api_data);
+			recipeArr.push(api_data);
+		});
+		return recipeArr;
 	}
 });
--- imports/components/Saved.js
+++ imports/components/Saved.js
@@ -3,7 +3,30 @@ import { Table, Tag } from 'antd';
 import RecipeTable from './search/RecipeTable.js';
 
 export default class Saved extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: null
+		};
+		this.getRecipeData();
+	}
+
+	getRecipeData() {
+		Meteor.callPromise('users.getSavedRecipe', Meteor.userId()).then(data => {
+			this.setState({ data });
+			this.forceUpdate();
+		});
+	}
+
 	render() {
-		return <div className="table" />;
+		const { data } = this.state;
+		if (!data) return <div> Loading </div>;
+
+		return (
+			<div className="table">
+				<RecipeTable data={data} />
+			</div>
+		);
 	}
 }
